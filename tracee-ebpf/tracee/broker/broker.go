d5384cfb61c37496dec477c5d4777c67167059ed
Michael Tcherniack <tcherniackmichael@gmail.com>
2021-06-28 14:50:03 +0300

    backup broker and streamer

--- tracee-ebpf/tracee/broker/broker.go
+++ tracee-ebpf/tracee/broker/broker.go
@@ -21,14 +21,15 @@ type Broker struct {
 func (b *Broker) Register(streamer streamers.Streamer) error {
 	nextAvailStreamerId := atomic.AddUint64(&b.nextAvailStreamerId, 1)
 	atomic.StoreUint64(&b.nextAvailStreamerId, nextAvailStreamerId)
-	if ok := b.Streamers.SetIfAbsent(strconv.FormatUint(nextAvailStreamerId, 10), streamer); !ok {
-		return fmt.Errorf("failed to subscribe streamer")
-	}
-	c := make(chan *external.Event, 100)
-	if ok := b.ChanPerStreamer.SetIfAbsent(strconv.FormatUint(nextAvailStreamerId, 10), c); !ok {
+	id := strconv.FormatUint(nextAvailStreamerId, 10)
+	if ok := b.Streamers.SetIfAbsent(id, streamer); !ok {
 		return fmt.Errorf("failed to subscribe streamer")
 	}
 
+	c := make(chan *external.Event, 100)
+	if ok := b.ChanPerStreamer.SetIfAbsent(id, c); !ok {
+		return fmt.Errorf("failed to subscribe streamer")
+	}
 	streamer.SetId(nextAvailStreamerId)
 	streamer.SetEventsChan(c)
 
@@ -42,10 +43,11 @@ func (b *Broker) Unregister(id uint64) (streamers.Streamer, error) {
 	if !ok {
 		return nil, fmt.Errorf("not existing subscriber: %v", id)
 	}
-	b.Streamers.Remove(strconv.FormatUint(id, 10))
-	c, _ := b.ChanPerStreamer.Get(strconv.FormatUint(id, 10))
+	idStr := strconv.FormatUint(id, 10)
+	b.Streamers.Remove(idStr)
+	c, _ := b.ChanPerStreamer.Get(idStr)
 	close(c.(chan *external.Event))
-	b.ChanPerStreamer.Remove(strconv.FormatUint(id, 10))
+	b.ChanPerStreamer.Remove(idStr)
 	return s.(streamers.Streamer), nil
 }
 
@@ -70,7 +72,7 @@ func (b *Broker) Start(stats *tracee.StatsStore) error {
 func (b *Broker) Stop(stats *tracee.StatsStore) {
 	// closing handler channel
 	cb := func(key string, v interface{}) {
-		close (v.(chan *external.Event))
+		close(v.(chan *external.Event))
 	}
 	b.ChanPerStreamer.IterCb(cb)
 	b.ChanPerStreamer.Clear()
